2f074e55fce82ebbe114fafcd17ea2a65fbf2847
Adam De Guire <adam@ardeguire.com>
2013-09-30 12:52:09 -0400

    Start js file. Make a pretty pattern with the board on load.

---
+++ project4/tictactoe/ttt.js
@@ -0,0 +1,25 @@
+function changeBoard(node, player) {
+  var marker = null;
+  var color = null;
+
+  if (player === 0) {
+    marker = 'X';
+    color = 'gray';
+  } else if (player === 1) {
+    marker = 'O';
+    color = 'red';
+  }
+  node.innerHTML = marker;
+  node.style.backgroundColor = color;
+}
+
+var player = 0;
+for (i = 0; i < 3; i++) {
+  for (j = 0; j < 3; j++) {
+    var id = 'b' + i + j;
+    node = document.getElementById(id);
+    changeBoard(node, player);
+    player = (player + 1) % 2;
+  }
+}
+
