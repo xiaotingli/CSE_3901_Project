d572121bd762b66c31c7bb934863d457302e83c0
Adam De Guire <adam@ardeguire.com>
2013-09-30 19:49:53 -0400

    More setup for the game loop.

--- project4/tictactoe/ttt.js
+++ project4/tictactoe/ttt.js
@@ -1,4 +1,4 @@
-function changeBoard(node, player) {
+function updateBoard(node, player) {
   var marker = node.innerHTML;
   var color = node.style.backgroundColor;
 
@@ -13,19 +13,30 @@ function changeBoard(node, player) {
   node.style.backgroundColor = color;
 }
 
-var player = 0
+function userTurn(node) {
+}
+
+
+var turn = document.getElementById('turn');
+var node = null;
+var player = 0;
 var gameOver = false;
 while (gameOver === false) {
   // switch players
   player = (player + 1) % 2;
 
-  // output which person's turn it is
+  // output which player's turn it is
+  turn.innerHTML = 'Player ' + player + ', it is your turn.';
   
   if (player === 0) {
     // make computer's move
   } else if (player === 1) {
     // wait for user's move
+    userTurn();
   }
+
+  updateBoard(node, player);
+  node = null;
 
   //test all winning cases
 }
