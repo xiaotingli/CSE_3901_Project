30131ed286d7bf24b33c80cc69cbd055fb001f87
Connor Humphries <tyedyehumps@gmail.com>
2013-10-02 01:17:35 -0400

    The O's Appear!

--- project4/tictactoe/ttt.js
+++ project4/tictactoe/ttt.js
@@ -1,41 +1,58 @@
 function updateBoard(node, player) {
-  var marker = node.innerHTML;
+  var marker = node.value;
   var color = node.style.backgroundColor;
 
-  if (player === 0) {
+  if (Number(player) === 0) {
     marker = 'X';
     color = 'gray';
-  } else if (player === 1) {
+  } else if (Number(player) === 1) {
     marker = 'O';
     color = 'red';
   }
-  node.innerHTML = marker;
+  
+  node.value += marker;
   node.style.backgroundColor = color;
 }
 
 function userTurn(node) {
+	playerTurn = 1;
+	updateBoard(node, playerTurn);
+	checkWin();
+	}
+
+function computerTurn()
+{
 }
 
+function checkWin()
+{
+	if(b00.style.backgroundColor === b01.style.backgroundColor === b02.style.backgroundColor === 'red')
+		{
+		turn.InnerHTML = 'PLAYER WINS!'
+		}
+}
 
 var turn = document.getElementById('turn');
 var node = null;
-var player = 0;
+var playerTurn = 0;
 var gameOver = false;
 while (gameOver === false) {
   // switch players
-  player = (player + 1) % 2;
+  playerTurn = (playerTurn + 1) % 2;
 
   // output which player's turn it is
   turn.innerHTML = 'Player ' + player + ', it is your turn.';
   
   if (player === 0) {
     // make computer's move
+	turn.innerHTML = 'The Computer is thinking.';
+	setTimeout(computerTurn(), 2);
+	
   } else if (player === 1) {
     // wait for user's move
     userTurn();
   }
 
-  updateBoard(node, player);
   node = null;
 
   //test all winning cases
